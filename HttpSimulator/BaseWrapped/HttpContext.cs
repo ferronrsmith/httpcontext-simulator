18cd18992776e8897e8f3384d494afdccb09555e
Oskar Gewalli <oskar@gewalli.se>
2014-03-11 19:43:56 +0100

    Some more tests
    For some reason mappath produces different result on mono

--- HttpSimulator/BaseWrapped/HttpContext.cs
+++ HttpSimulator/BaseWrapped/HttpContext.cs
@@ -6,11 +6,12 @@ namespace Http.TestLibrary.BaseWrapped
     {
         private readonly HttpRequestBase _workerRequest;
         private readonly HttpSessionStateBase _fakeHttpSessionState;
-
-        public HttpContext(HttpRequestBase workerRequest, HttpSessionStateBase fakeHttpSessionState)
+        private readonly HttpServerUtility _fakeHttpServerUtility;
+        public HttpContext(HttpRequestBase workerRequest, HttpSessionStateBase fakeHttpSessionState, HttpServerUtility fakeHttpServerUtility)
         {
             _workerRequest = workerRequest;
             _fakeHttpSessionState = fakeHttpSessionState;
+            _fakeHttpServerUtility = fakeHttpServerUtility;
         }
 
         public override HttpSessionStateBase Session
@@ -21,6 +22,11 @@ namespace Http.TestLibrary.BaseWrapped
         public override HttpRequestBase Request
         {
             get { return _workerRequest; }
+        }
+
+        public override HttpServerUtilityBase Server
+        {
+            get { return _fakeHttpServerUtility; }
         }
     }
 }
